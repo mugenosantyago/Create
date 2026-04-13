c4e131fb06b259274cc6d84df101edae4d389980
mugenosantyago <147428965+mugenosantyago@users.noreply.github.com>
2026-04-13 18:04:37 -0500

    Update CreateParticleProviders.java

--- src/main/java/com/simibubi/create/foundation/particle/CreateParticleProviders.java
+++ src/main/java/com/simibubi/create/foundation/particle/CreateParticleProviders.java
@@ -2,14 +2,23 @@ package com.simibubi.create.foundation.particle;
 
 import com.simibubi.create.AllParticleTypes;
 import com.simibubi.create.content.equipment.bell.SoulBaseParticle;
+import com.simibubi.create.content.equipment.bell.SoulBaseParticleData;
 import com.simibubi.create.content.equipment.bell.SoulParticle;
+import com.simibubi.create.content.equipment.bell.SoulParticleData;
+import com.simibubi.create.content.equipment.bell.SoulParticleExpandingPerimeterData;
+import com.simibubi.create.content.equipment.bell.SoulParticlePerimeterData;
 import com.simibubi.create.content.fluids.particle.FluidParticleData;
 import com.simibubi.create.content.fluids.particle.FluidStackParticle;
 import com.simibubi.create.content.kinetics.base.RotationIndicatorParticle;
+import com.simibubi.create.content.kinetics.base.RotationIndicatorParticleData;
 import com.simibubi.create.content.kinetics.fan.AirFlowParticle;
+import com.simibubi.create.content.kinetics.fan.AirFlowParticleData;
 import com.simibubi.create.content.kinetics.steamEngine.SteamJetParticle;
+import com.simibubi.create.content.kinetics.steamEngine.SteamJetParticleData;
 import com.simibubi.create.content.logistics.packagerLink.WiFiParticle;
+import com.simibubi.create.content.logistics.packagerLink.WiFiParticleData;
 import com.simibubi.create.content.trains.CubeParticle;
+import com.simibubi.create.content.trains.CubeParticleData;
 
 import net.minecraft.client.particle.ParticleProvider;
 import net.minecraft.core.particles.ParticleType;
@@ -24,28 +33,32 @@ public final class CreateParticleProviders {
 
 	private CreateParticleProviders() {}
 
+	@SuppressWarnings("unchecked")
 	public static void register(RegisterParticleProvidersEvent event) {
-		event.registerSpriteSet(AllParticleTypes.ROTATION_INDICATOR.get(), RotationIndicatorParticle.Factory::new);
-		event.registerSpriteSet(AllParticleTypes.AIR_FLOW.get(), AirFlowParticle.Factory::new);
-		event.registerSpriteSet(AllParticleTypes.AIR.get(), AirParticle.Factory::new);
-		event.registerSpriteSet(AllParticleTypes.STEAM_JET.get(), SteamJetParticle.Factory::new);
-		event.registerSpecial(AllParticleTypes.CUBE.get(), new CubeParticle.Factory());
+		event.registerSpriteSet((ParticleType<RotationIndicatorParticleData>) AllParticleTypes.ROTATION_INDICATOR.get(),
+			RotationIndicatorParticle.Factory::new);
+		event.registerSpriteSet((ParticleType<AirFlowParticleData>) AllParticleTypes.AIR_FLOW.get(),
+			AirFlowParticle.Factory::new);
+		event.registerSpriteSet((ParticleType<AirParticleData>) AllParticleTypes.AIR.get(), AirParticle.Factory::new);
+		event.registerSpriteSet((ParticleType<SteamJetParticleData>) AllParticleTypes.STEAM_JET.get(),
+			SteamJetParticle.Factory::new);
+		event.registerSpecial((ParticleType<CubeParticleData>) AllParticleTypes.CUBE.get(), new CubeParticle.Factory());
 
 		ParticleProvider<FluidParticleData> fluidProvider = (data, world, x, y, z, vx, vy, vz) -> FluidStackParticle
 			.create((ParticleType<FluidParticleData>) data.getType(), world, data.getFluid(), x, y, z, vx, vy, vz);
-		event.registerSpecial(AllParticleTypes.FLUID_PARTICLE.get(), fluidProvider);
-		event.registerSpecial(AllParticleTypes.BASIN_FLUID.get(), fluidProvider);
-		event.registerSpecial(AllParticleTypes.FLUID_DRIP.get(), fluidProvider);
+		event.registerSpecial((ParticleType<FluidParticleData>) AllParticleTypes.FLUID_PARTICLE.get(), fluidProvider);
+		event.registerSpecial((ParticleType<FluidParticleData>) AllParticleTypes.BASIN_FLUID.get(), fluidProvider);
+		event.registerSpecial((ParticleType<FluidParticleData>) AllParticleTypes.FLUID_DRIP.get(), fluidProvider);
 
-		event.registerSpriteSet(AllParticleTypes.WIFI.get(),
+		event.registerSpriteSet((ParticleType<WiFiParticleData>) AllParticleTypes.WIFI.get(),
 			sprites -> (data, w, x, y, z, vx, vy, vz) -> new WiFiParticle(w, x, y, z, vx, vy, vz, sprites));
-		event.registerSpriteSet(AllParticleTypes.SOUL.get(),
+		event.registerSpriteSet((ParticleType<SoulParticleData>) AllParticleTypes.SOUL.get(),
 			sprites -> (data, w, x, y, z, vx, vy, vz) -> new SoulParticle(w, x, y, z, vx, vy, vz, sprites, data));
-		event.registerSpriteSet(AllParticleTypes.SOUL_PERIMETER.get(),
+		event.registerSpriteSet((ParticleType<SoulParticlePerimeterData>) AllParticleTypes.SOUL_PERIMETER.get(),
 			sprites -> (data, w, x, y, z, vx, vy, vz) -> new SoulParticle(w, x, y, z, vx, vy, vz, sprites, data));
-		event.registerSpriteSet(AllParticleTypes.SOUL_EXPANDING_PERIMETER.get(),
+		event.registerSpriteSet((ParticleType<SoulParticleExpandingPerimeterData>) AllParticleTypes.SOUL_EXPANDING_PERIMETER.get(),
 			sprites -> (data, w, x, y, z, vx, vy, vz) -> new SoulParticle(w, x, y, z, vx, vy, vz, sprites, data));
-		event.registerSpriteSet(AllParticleTypes.SOUL_BASE.get(),
+		event.registerSpriteSet((ParticleType<SoulBaseParticleData>) AllParticleTypes.SOUL_BASE.get(),
 			sprites -> (data, w, x, y, z, vx, vy, vz) -> new SoulBaseParticle(w, x, y, z, vx, vy, vz, sprites));
 	}
 }
