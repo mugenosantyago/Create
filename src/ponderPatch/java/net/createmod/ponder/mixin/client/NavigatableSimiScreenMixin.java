e4d5a1eb571ab719ca609403c6c652fa9ede622d
mugenosantyago <147428965+mugenosantyago@users.noreply.github.com>
2026-05-02 16:18:34 -0500

    i

--- src/ponderPatch/java/net/createmod/ponder/mixin/client/NavigatableSimiScreenMixin.java
+++ src/ponderPatch/java/net/createmod/ponder/mixin/client/NavigatableSimiScreenMixin.java
@@ -1,10 +1,11 @@
-package net.createmod.catnip.gui;
+package net.createmod.ponder.mixin.client;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import net.createmod.catnip.gui.NavigatableSimiScreen;
 import net.minecraft.client.gui.GuiGraphics;
 
 /**
