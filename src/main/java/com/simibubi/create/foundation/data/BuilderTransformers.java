d7d487f9659050e8bb92e07f965921ff9922f106
mugenosantyago <147428965+mugenosantyago@users.noreply.github.com>
2026-05-07 11:37:00 -0500

    Update BuilderTransformers.java

--- src/main/java/com/simibubi/create/foundation/data/BuilderTransformers.java
+++ src/main/java/com/simibubi/create/foundation/data/BuilderTransformers.java
@@ -417,7 +417,7 @@ public class BuilderTransformers {
 			ItemBuilder<TableClothBlockItem, BlockBuilder<B, P>> item = b.initialProperties(initialProps)
 				.addLayer(() -> () -> net.minecraft.client.renderer.chunk.ChunkSectionLayer.CUTOUT_MIPPED)
 				.defaultBlockstate()
-				.onRegister(CreateRegistrate.blockModel())
+				.onRegister(CreateRegistrate.blockModel(TableClothModel::new))
 				.tag(AllBlockTags.TABLE_CLOTHS.tag, soundTag)
 				.onRegisterAfter(Registries.ITEM, v -> ItemDescription.useKey(v, "block.create.table_cloth"))
 				.item(TableClothBlockItem::new);
