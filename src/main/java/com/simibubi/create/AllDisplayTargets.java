49d5fae3dc8f90a5641515844bdbcc92768b4feb
mugenosantyago <147428965+mugenosantyago@users.noreply.github.com>
2026-05-08 06:24:40 -0500

    f

--- src/main/java/com/simibubi/create/AllDisplayTargets.java
+++ src/main/java/com/simibubi/create/AllDisplayTargets.java
@@ -13,27 +13,27 @@ import java.util.Map;
 import java.util.function.Supplier;
 
 public class AllDisplayTargets {
-	private static final CreateRegistrate REGISTRATE = Create.registrate();
+	private static final CreateRegistrate REGISTRATE = net.neoforged.api.distmarker.Dist.CLIENT == net.neoforged.fml.loading.FMLEnvironment.dist ? Create.registrate() : null;
 
-	public static final RegistryEntry<DisplayTarget, SignDisplayTarget> SIGN = REGISTRATE.displayTarget("sign", SignDisplayTarget::new)
+	public static final RegistryEntry<DisplayTarget, SignDisplayTarget> SIGN = REGISTRATE != null ? REGISTRATE.displayTarget("sign", SignDisplayTarget::new)
 		.associate(BlockEntityType.SIGN)
-		.register();
-	public static final RegistryEntry<DisplayTarget, LecternDisplayTarget> LECTERN = REGISTRATE.displayTarget("lectern", LecternDisplayTarget::new)
+		.register() : null;
+	public static final RegistryEntry<DisplayTarget, LecternDisplayTarget> LECTERN = REGISTRATE != null ? REGISTRATE.displayTarget("lectern", LecternDisplayTarget::new)
 		.associate(BlockEntityType.LECTERN)
-		.register();
+		.register() : null;
 
 	public static final RegistryEntry<DisplayTarget, DisplayBoardTarget> DISPLAY_BOARD = simple("display_board", DisplayBoardTarget::new);
 	public static final RegistryEntry<DisplayTarget, NixieTubeDisplayTarget> NIXIE_TUBE = simple("nixie_tube", NixieTubeDisplayTarget::new);
 
-	public static final Map<String, RegistryEntry<DisplayTarget, ? extends DisplayTarget>> LEGACY_NAMES = Map.of(
+	public static final Map<String, RegistryEntry<DisplayTarget, ? extends DisplayTarget>> LEGACY_NAMES = REGISTRATE != null ? Map.of(
 		"sign_display_target", SIGN,
 		"lectern_display_target", LECTERN,
 		"display_board_target", DISPLAY_BOARD,
 		"nixie_tube_target", NIXIE_TUBE
-	);
+	) : Map.of();
 
 	private static <T extends DisplayTarget> RegistryEntry<DisplayTarget, T> simple(String name, Supplier<T> supplier) {
-		return REGISTRATE.displayTarget(name, supplier).register();
+		return REGISTRATE != null ? REGISTRATE.displayTarget(name, supplier).register() : null;
 	}
 
 	public static void register() {
